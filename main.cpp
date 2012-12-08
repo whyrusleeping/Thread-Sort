d3f99e2f28baaefa18d1948aaf78388fc0d31588
Jeromy <jeromyj@gmail.com>
2012-12-07 20:07:36 -0800

    updated timing and added optimization

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "ThrMergeSort.h"
 #include "yardstick.h"
+#include <sys/time.h>
 #include <cstdlib>
 #include <iostream>
 
@@ -16,19 +17,28 @@ int main(int argc, char *argv[])
 		myar[i] = rand(); 
 	vector<int> myar2(myar);
 	
-	//Initialize timers
+	///Initialize timers
+	//CPU time
 	Yardstick y1;
 	Yardstick y2;
+	//Real Time
+	struct timeval begin, mid, end;
 
+	gettimeofday(&begin, NULL);
 	y1.start();
 	ThreadSort(&myar2);
 	struct timespec t1 = y1.end();
+	gettimeofday(&mid, NULL);
 	y2.start();
 	mergeSort(&myar);
 	struct timespec t2 = y2.end();
+	gettimeofday(&end, NULL);
 
-	cout << "Threadsort:\t" << (t1.tv_sec * 1000000000) + t1.tv_nsec << "\n";
+	double threadsortTime = (double) (mid.tv_usec - begin.tv_usec)/1000000 + (double) (mid.tv_sec - begin.tv_sec);
+	double mergesortTime = (double) (end.tv_usec - mid.tv_usec)/1000000 + (double) (end.tv_sec - mid.tv_sec);
+
+	cout << "Threadsort:\t\nCPU time: " << (t1.tv_sec * 1000000000) + t1.tv_nsec << "\nReal Time: " << threadsortTime << "\n";
 	
-	cout << "Mergesort:\t" << (t2.tv_sec * 1000000000) + t2.tv_nsec << "\n";
+	cout << "Mergesort:\t\nCPU time: " << (t2.tv_sec * 1000000000) + t2.tv_nsec << "\nReal Time: " << mergesortTime << "\n";
 	return 0;
 }
