c66f760c46e0ca0923acfa29ff5d59dc3ec1a825
Jeromy <jeromyj@gmail.com>
2012-12-07 21:05:06 -0800

    cleaned up code

--- ThrMergeSort.cpp
+++ ThrMergeSort.cpp
@@ -3,26 +3,38 @@
 
 void ThreadSort(vector<int> *arr)
 {
+	//Call _Threadsort with appropriate parameters.
 	_ThreadSort(arr, 0, (*arr).size()-1); 
 }
 
 void _ThreadSort(vector<int> *arr, int start, int end)
 {
+		//Find middle of array
 		int mid = (end - start) / 2;
+
+		//Break array into four segments by finding secondary midpoints
 		int midl = mid / 2;
 		int midr = (mid / 2) + mid;
+
+		//Create thread to mergesort each of those four segments
 		thread tl(_mergeSort, arr, start, midl);
 		thread tlm (_mergeSort, arr, midl + 1, mid);
 		thread trm (_mergeSort, arr, mid + 1, midr);
 		thread tr(_mergeSort, arr, midr + 1, end);
+
+		//Wait for each thread to finish
 		tl.join();
 		tr.join();
 		tlm.join();
 		trm.join();
+
+		//merge left halves and right halves asynchronously
 		thread mergeTl(merge, arr, start, midl, mid);
 		thread mergeTr(merge, arr, mid + 1, midr, end);
 		mergeTl.join();
 		mergeTr.join();
+
+		//finally merge left with right
 		merge(arr, start, mid, end);
 }
 
@@ -31,22 +43,19 @@ void mergeSort(vector<int> *arr)
 	_mergeSort(arr,0,(*arr).size()-1);
 }
 
+//Standard mergesort algorithm, sort left, sort right, merge.
 void _mergeSort(vector<int> *arr, int start, int end)
 {
-	//cout << "mergesort: " << start << " to " << end << "\n";
-	int mid = (end + start) / 2;
-	//for(int i = start; i < end; i++)
-	//	cout << (*arr)[i] << " ";
-	//cout << "\n";
-	//cout << "mid: " << mid << "\n\n";
 	if(start < end)
 	{
+		int mid = (end + start) / 2;
 		_mergeSort(arr, start, mid);
 		_mergeSort(arr, mid + 1, end);
 		merge(arr, start, mid, end);
 	}
 }
 
+//Merge two vectors into one in sorted order
 void merge(vector<int> *arr, int start, int mid, int end)
 {
 	vector<int> sorted((end - start) + 1);
--- ThrMergeSort.h
+++ ThrMergeSort.h
@@ -8,10 +8,19 @@ using std::thread;
 using std::cout;
 using std::vector;
 
+//Main call for threadsort
 void ThreadSort(vector<int> *arr);
+
+//'Recursive' call for threadsort (i removed recursion on this due to performance issues)
 void _ThreadSort(vector<int> *arr, int start, int end);
-void _mergeSort(vector<int> *arr, int start, int end);
+
+//Main call for mergesort (mostly just for comparison)
 void mergeSort(vector<int> *arr);
+
+//Recursive mergesort call
+void _mergeSort(vector<int> *arr, int start, int end);
+
+//Merge arr[start:mid] with arr[mid+1:end]
 void merge(vector<int> *arr, int start, int mid, int end);
 
 #endif
